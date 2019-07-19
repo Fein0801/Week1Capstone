683ebc3b091a27ccbb93a27e6ceb091be96e53de
Fein0801 <ben.e.feinstein@gmail.com>
2019-07-19 15:22:25 -0400

    Minimum viable product

--- src/co/grandcircus/PigLatinTranslator.java
+++ src/co/grandcircus/PigLatinTranslator.java
@@ -1,13 +1,13 @@
 package co.grandcircus;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class PigLatinTranslator {
 
     // "List" of vowels in English
     private static final String[] VOWELS = { "a", "e", "i", "o", "u" };
+    private static final String[] IGNORE = {};
 
     // "List" of indices of first occurence of each vowel
     private static int[] vowelIndices = new int[VOWELS.length];
@@ -16,31 +16,11 @@ public class PigLatinTranslator {
 	Scanner scan = new Scanner(System.in);
 	String output = "";
 
-	ArrayList<String> lineComponents = new ArrayList<String>();
-
-	String[] testStrings = { "Hello my baby", "Hello my honey", "open the door" };
-	String[] testWords = { "this", "hello", "WORD", "Benjamin" };
-
-	findVowels(testWords[3].toLowerCase());
-
-	System.out.println(Arrays.toString(vowelIndices));
-	System.out.println(findFirstVowel(testWords[3].toLowerCase()));
-
 	System.out.println("Please enter a sentence to translate.");
 	String input = scan.nextLine();
 
 	// If line contains spaces, split line and add to lineComponents
-	if (input.contains(" ")) {
-	    String[] arr = input.split(" ");
-	    for (String s : arr) {
-		lineComponents.add(s);
-	    }
-	} else {
-	    lineComponents.add(input);
-	}
-
-	// Clear the list
-	lineComponents.clear();
+	output = translateLine(input);
 
 	System.out.println(output);
 
@@ -52,6 +32,7 @@ public class PigLatinTranslator {
 	int firstVowelIndex = -1;
 	for (int i = 0; i < vowelIndices.length; i++) {
 	    int currIndex = vowelIndices[i];
+
 	    if ((firstVowelIndex == -1 && currIndex != -1)) {
 		firstVowelIndex = currIndex;
 	    }
@@ -62,16 +43,55 @@ public class PigLatinTranslator {
 	return firstVowelIndex;
     }
 
-    // Finds the FIRST instance of each vowel in str //Never gets called in main
+    // Finds the FIRST instance of each vowel in str (Never gets called in main)
     private static void findVowels(String str) {
 	for (int i = 0; i < VOWELS.length; i++) {
 	    vowelIndices[i] = str.indexOf(VOWELS[i]);
 	}
     }
 
-    private static String translateWord(String str, int firstVowelIndex) {
+    private static String translateWord(String str) {
+	int firstVowelIndex = findFirstVowel(str);
+	ArrayList<String> ignore = new ArrayList<String>();
 
-	return ""; // FIXME actually do something
+	for (String s : IGNORE) { // TODO may help me implement certain things, I don't know though
+	    ignore.add(s);
+	}
+
+	// I want to ignore certain words
+	if (!ignore.contains(str) && str.length() > 2) {
+	    if (firstVowelIndex == -1) {
+		return str;
+	    } else if (firstVowelIndex == 0) {
+		return str.concat("way");
+	    } else {
+		String firstConsonants = str.substring(0, firstVowelIndex);
+		return str.substring(firstVowelIndex).concat(firstConsonants).concat("ay");
+	    }
+	}
+	return str;
+    }
+
+    private static String translateLine(String str) {
+	ArrayList<String> lineComponents = new ArrayList<String>();
+
+	String finalLine = "";
+	if (str.contains(" ")) {
+	    String[] arr = str.split(" ");
+	    for (String s : arr) {
+		lineComponents.add(s);
+	    }
+	} else {
+	    lineComponents.add(str);
+	}
+
+	for (String s : lineComponents) {
+	    finalLine = finalLine.concat(translateWord(s)).concat(" ");
+	}
+	// Clear the list
+	lineComponents.clear();
+
+	return finalLine; // FIXME return something
     }
 
 }
